615ce9ce31afff44c500b2060ee4b53ee6ffa379
Valerie Williams <valerielwilliams@ufl.edu>
2017-12-17 13:32:42 -0500

    concatenate JavaScript and optimize form

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,34 +1,80 @@
-//= include ../../../bower_components/jquery/dist/jquery.js
-//= include ../../../bower_components/jquery.fitvids/jquery.fitvids.js
+//@prepros-prepend jquery-3.2.1.js
+//@prepros-prepend backtotop.js
+//@prepros-prepend production.js
 
-jQuery ( function($) {
+jQuery ( function ($) {
 
-	var site = new SiteController($);
-	site.init();
+    var site = new SiteController($);
+    site.init();
 
 });
 
 function SiteController($)
 {
-	self.init = function()
-	{
-		initMobileNav();
-	}
+    self.init = function()
+    {
+        initMobileNav();
+        initHandleLabels();
+    }
 
-	function initMobileNav() {
-		// CLICK ON TRIGGER, OPEN THE MOBILE NAV
-		$('.js-mobile-trigger').click(function(e) {
-			e.preventDefault();
-		$('.js-mobile-nav').addClass('open');
-		});
+    function initMobileNav() {
 
+        //CLICK ON TRIGGER, OPEN THE MOBILE NAV
+        $('.js-mobile-trigger').click(function(e) {
+            e.preventDefault();
+            $('.js-mobile-nav').addClass('open');
+        });
 
-		//CLICK ON CLOSE BUTTON, CLOSE MOBILE NAV
-		$('.js-mobile-nav-close').click(function(e) {
-			e.preventDefault();
-		$('.js-mobile-nav').removeClass('open');
-		});
-	}
+        //CLICK ON CLOSE BUTTON, CLOSE MOBILE NAV
+        $('.js-mobile-nav-close').click(function(e) {
+            e.preventDefault();
+            $('.js-mobile-nav').removeClass('open');
+        });
+    }
 
-	return self;
+    function initHandleLabels() {
+        $('input[type=text]').focus(function(e) {
+            $(this).parent().find('label').addClass('out-of-the-way');
+        });
+
+        $('input[type=text]').blur(function(e) {
+
+            //if the field has a value, leave it
+            if ($(this).val() || $(this).val() != '') {
+                return;
+            }
+            $(this).parent().find('label').removeClass('out-of-the-way');
+            //otherwise, move the label back
+        });
+
+        $('input[type=phone]').focus(function(e) {
+            $(this).parent().find('label').addClass('out-of-the-way');
+        });
+
+        $('input[type=phone]').blur(function(e) {
+
+            //if the field has a value, leave it
+            if ($(this).val() || $(this).val() != '') {
+                return;
+            }
+            $(this).parent().find('label').removeClass('out-of-the-way');
+            //otherwise, move the label back
+        });
+
+        $('textarea').focus(function(e) {
+            $(this).parent().find('label').addClass('out-of-the-way');
+        });
+
+        $('textarea').blur(function(e) {
+
+            //if the field has a value, leave it
+            if ($(this).val() || $(this).val() != '') {
+                return;
+            }
+            $(this).parent().find('label').removeClass('out-of-the-way');
+            //otherwise, move the label back
+        });
+    }
+
+return self;
 }
